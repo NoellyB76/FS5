0b517342a18aa5bba825fe914c29534ad5d0db3d
NoellyB76 <noelrcs@gmail.com>
2015-05-26 11:39:50 -0400

    Commit 2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,6 +13,43 @@ angular.module('starter.controllers', [])
   $scope.furnitureItem = FurnitureItems.get($stateParams.furnitureItemId);
 })
 
+
+.controller('ImgAccordion', function($scope) {
+
+  $scope.groups = [];
+  $scope.groups[0] = {
+    name: 'Frame Finish',
+    items:  ["antique black walnut", "antique black", "antique dark rum"]
+  };
+
+$scope.groups[1] = {
+  name: 'Main Fabric',
+  items: ["belle sapphire", "canvas cork", "canyon bamboo"]
+};
+
+$scope.groups[2] = {
+  name: "Kidney Fabric",
+  items: ["belle sapphire", "canvas cork", "canyon bamboo"]
+};
+
+
+  $scope.toggleGroup = function(group) {
+    if ($scope.isGroupShown(group)) {
+      $scope.shownGroup = null;
+    } else {
+      $scope.shownGroup = group;
+    }
+  };
+  $scope.isGroupShown = function(group) {
+    return $scope.shownGroup === group;
+  };
+
+})
+
+
+
+
+
 .controller('AccountCtrl', function($scope) {
   $scope.settings = {
     enableFriends: true
